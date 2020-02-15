90c6029e0e18d07a9ad3a06b8bd5339bccde8615
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2020-02-15 14:19:59 -0500

    Added rest of activities

---
+++ app/src/main/java/com/mrfoster/software_wolf/LynchActivity.java
@@ -0,0 +1,14 @@
+package com.mrfoster.software_wolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LynchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lynch);
+    }
+}
